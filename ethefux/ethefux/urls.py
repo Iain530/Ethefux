5e8a6d58715e8f585ed6833dfeadbf66a8be6d0b
Iain <2189570c@student.gla.ac.uk>
2017-10-07 10:00:10 +0100

    urls setup

--- ethefux/ethefux/urls.py
+++ ethefux/ethefux/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ethefux_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^', include('ethefux_app.urls')),
+    url(r'^ethefux/', include('ethefux_app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ ethefux/ethefux_app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from ethefux_app import views
+
+app_name = 'ethefux_app'
+
+urlpatterns = [
+    
+]
