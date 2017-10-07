b9257d3018d59f638853643a1036f8ac86fe6384
Iain Cattermole <iain_c@live.co.uk>
2017-10-07 12:14:06 +0100

    basic html home page with base.html

--- ethefux/ethefux_app/views.py
+++ ethefux/ethefux_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+def home(request):
+	context_dict = {}
+	return render(request, 'ethefux_app/home.html', context_dict)
