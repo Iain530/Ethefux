1957c397573aabdde5cb73fca5e738751187ae30
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-10-07 12:38:06 +0100

    Contract proposal and confirmation models

--- ethefux/ethefux_app/models.py
+++ ethefux/ethefux_app/models.py
@@ -26,10 +26,20 @@ class UserProfile(models.Model):
 
         super(UserProfile, self).save()
 
-
+# For keeping track of wallets
 class Wallet(models.Model):
     address = models.CharField(max_length=42, default="0x255750c6f883f5d470ea7ed4a9a33ca56c890cd8")
 
-
+# A deployed contract
 class Contract(models.Model):
     address = models.CharField(max_length=42)
+    parties = models.ManyToManyField("UserProfile")
+
+# A proposed contract between some parties, will be deployed after all parties confirm
+class ContractProposal(models.Model):
+    partyconfirmations = models.ManyToManyField("DeployConfirmation")
+    
+# A confirmation by a party for a contract
+class DeployConfirmation(models.Model):
+    contract = models.ForeignKey("Contract")
+    confirmer = models.ForeignKey("UserProfile")
