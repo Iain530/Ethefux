25cc89088293ce4d001bb8b3543d203f60b2f26b
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-10-08 09:52:22 +0100

    Fixed Loan form properties

--- ethefux/ethefux_app/forms.py
+++ ethefux/ethefux_app/forms.py
@@ -1,13 +1,18 @@
 from django import forms
+from django.core.validators import EmailValidator
 
 class LoanForm(forms.Form):
     # The other side of the loan
-    party = forms.EmailField()
+    party = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Email of other party', 'autofocus':'1'}),
+                            max_length=150, validators=[EmailValidator])
 
-    amount = forms.DecimalField()
+    amount = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Loan Amount'}))
     
     # Duration in months
-    duration = forms.IntegerField()
+    duration = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Duration (Months)'}))
 
     # In decimal format, 1.01 for 1% interest, etc
-    interest_rate = forms.DecimalField()
+    interest_rate = forms.DecimalField(widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':"Interest Rate (Monthly)"}))
+
+    class Meta:
+        fields = ('party', 'amount', 'duration', 'interest_rate')
