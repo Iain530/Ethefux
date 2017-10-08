d0fcee6d5b2a84b535a4410c651476c1d61eee29
Iain Cattermole <iain_c@live.co.uk>
2017-10-08 13:05:02 +0100

    errors work

--- ethefux/ethefux_app/views.py
+++ ethefux/ethefux_app/views.py
@@ -176,25 +176,21 @@ def deploy_contract(request):
 
 # ERROR PAGS
 def handler400(request):
-    response = render_to_response('errors/400.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('errors/400.html', {})
     response.status_code = 400
     return response
 
 def handler404(request):
-    response = render_to_response('errors/403.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('errors/403.html', {})
     response.status_code = 403
     return response
 
 def handler404(request):
-    response = render_to_response('errors/404.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('errors/404.html', {})
     response.status_code = 404
     return response
 
 def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('500.html', {})
     response.status_code = 500
     return response
