5760dd52f8c01d3a11d508ec2a1da2f5acc879a4
Iain Cattermole <iain_c@live.co.uk>
2017-10-08 15:30:38 +0100

    go green pls

--- ethefux/static/js/jquery.js
+++ ethefux/static/js/jquery.js
@@ -1,5 +1,12 @@
 $(document).ready( function () {
-	
 
-	
+	$(".accept-contract").on("click", function() {
+		alert("click");
+		var div = ".contract > "+ $(this);
+		$(div).css("color", "#90ff68");
+		$(this).hide();
+		$(div).html += "Accepted";
+
+	});
+
 });
