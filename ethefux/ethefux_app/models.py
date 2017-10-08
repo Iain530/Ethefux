db8afab146a826e76838eb848ac14be415669843
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-10-08 10:10:28 +0100

    Removed userprofile autogen

--- ethefux/ethefux_app/models.py
+++ ethefux/ethefux_app/models.py
@@ -53,9 +53,3 @@ class DeployConfirmation(models.Model):
     contract = models.ForeignKey("ContractProposal")
     confirmer = models.ForeignKey("UserProfile")
     confirmed = models.BooleanField(default=False)
-
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-post_save.connect(create_user_profile, sender=User)
